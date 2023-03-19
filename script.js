9c6d26bdb63948d0a1242a9bb9eea2612e7a01d6
Johnny1352 <yonimalca@gmail.com>
2023-03-19 14:04:18 +0200

    added selectors

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const billInput = document.querySelector(".bill-input");
+const tipBtns = document.querySelectorAll(".tip-btn");
+const peopleInput = document.querySelector(".people-input");
+const tipAmount = document.querySelector(".tip-amount");
+const totalAmount = document.querySelector(".total-amount");
